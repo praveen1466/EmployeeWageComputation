1dcae6377f63121c73e8755bc5ea1c12acd7acc9
praveen1466 <praveensknl7@gmail.com>
2020-11-27 09:35:34 -0800

    Added: Employe is present or absent

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,5 +1,13 @@
+import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+   Random rand=new Random();
+   int attendance=rand.nextInt(2);
+   System.out.println("Staus of Employee Whether Employee is present or absent:"+attendance);
+   if(attendance == 1)
+   System.out.println("Employee is present...");
+   else
+   System.out.println("Employee is absent..");
   }
 }
