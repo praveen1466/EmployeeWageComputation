7116920f87e3c7fb2a3392f729145e78f1bff460
praveen1466 <praveensknl7@gmail.com>
2020-11-27 10:00:40 -0800

    Added: Employee monthly wages is updated

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,35 +2,35 @@ import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-	
+
    int empHrs;
    int salary;
    int IS_PART_TIME=1;
    int IS_FULL_TIME=2;
    int EMP_RATE_PER_HR=20;
-   
-   Random rand=new Random();
-   int empAttendance=rand.nextInt(3);
-   System.out.println("Staus of Employee Whether Employee is present or absent:"+empAttendance);
-   switch (empAttendance) {
-   case 1:
+   int NUM_WORKING_DAYS=20;
+   int totalSalary=0;
+
+   for(int day=1;day<=NUM_WORKING_DAYS;day++ )
+   {
+    Random rand=new Random();
+    int empAttendance=rand.nextInt(3);
+    switch (empAttendance) {
+    case 1:
            empHrs=4;
-           System.out.println("Employee hours: "+empHrs);
            salary=(empHrs*EMP_RATE_PER_HR);
-           System.out.println("Salary: "+salary);
            break;
-   case 2:
+    case 2:
            empHrs=8;
-           System.out.println("Employee hours: "+empHrs);
            salary=(empHrs*EMP_RATE_PER_HR);
-           System.out.println("Salary: "+salary);
            break;
-   default:
+    default:
            empHrs=0;
-           System.out.println("Employee hours: "+empHrs);
            salary=(empHrs*EMP_RATE_PER_HR);
-           System.out.println("Salary: "+salary);
            break;
     }
-  }
+    totalSalary=(totalSalary+salary);
+   }
+   System.out.println("Total Salary: "+totalSalary); 
+}
 }
