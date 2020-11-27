710e95edf9e193e2c545388a89c71b4d42a05bb5
praveen1466 <praveensknl7@gmail.com>
2020-11-27 09:42:41 -0800

    Added: Employe wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,12 +2,23 @@ import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+   int isPresent=1;
+   int salary;
    Random rand=new Random();
-   int attendance=rand.nextInt(2);
-   System.out.println("Staus of Employee Whether Employee is present or absent:"+attendance);
-   if(attendance == 1)
-   System.out.println("Employee is present...");
-   else
-   System.out.println("Employee is absent..");
+   int empAttendance=rand.nextInt(2);
+   System.out.println("Staus of Employee Whether Employee is present or absent:"+empAttendance);
+   if(empAttendance == isPresent) {
+
+        System.out.println("Employee is present...");
+        int empHrs=8;
+        int ratePerHour=20;
+        salary=empHrs * ratePerHour;
+   }
+   else {
+
+        System.out.println("Employee is absent...");
+        salary=0;
+   }
+  System.out.println("Salary of Employee: "+salary);
   }
 }
