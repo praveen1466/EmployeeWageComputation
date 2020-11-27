1cd618f8dadcd4932c0ddd8b474654ce5f34a3a2
praveen1466 <praveensknl7@gmail.com>
2020-11-27 09:49:18 -0800

    Added: Employee partime wage

--- empWageComputation.java
+++ empWageComputation.java
@@ -2,23 +2,24 @@ import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
    System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-   int isPresent=1;
-   int salary;
+   int empHrs;
+   int IS_PART_TIME=1;
+   int IS_FULL_TIME=2;
+   int EMP_RATE_PER_HR=20;
    Random rand=new Random();
    int empAttendance=rand.nextInt(2);
    System.out.println("Staus of Employee Whether Employee is present or absent:"+empAttendance);
-   if(empAttendance == isPresent) {
-
-        System.out.println("Employee is present...");
-        int empHrs=8;
-        int ratePerHour=20;
-        salary=empHrs * ratePerHour;
+   if(empAttendance == IS_PART_TIME) {
+        empHrs=4;
    }
-   else {
-
-        System.out.println("Employee is absent...");
-        salary=0;
+   else if(empAttendance == IS_FULL_TIME) {
+			empHrs=8;
    }
-  System.out.println("Salary of Employee: "+salary);
+	else
+			empHrs=0;
+
+ 
+  int salary=empHrs * EMP_RATE_PER_HR;
+  System.out.println("Salary: "+salary);
   }
 }
