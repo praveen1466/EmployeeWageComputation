874c217a32fe3b7dc0d267f9b3cd051ccaed27cb
praveen1466 <praveensknl7@gmail.com>
2020-11-27 09:55:09 -0800

    Added: Employee wages is updated using switch case

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,25 +1,36 @@
 import java.util.Random;
 public class empWageComputation {
 public static void main(String[] args) {
-   System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+	
    int empHrs;
+   int salary;
    int IS_PART_TIME=1;
    int IS_FULL_TIME=2;
    int EMP_RATE_PER_HR=20;
+   
    Random rand=new Random();
-   int empAttendance=rand.nextInt(2);
+   int empAttendance=rand.nextInt(3);
    System.out.println("Staus of Employee Whether Employee is present or absent:"+empAttendance);
-   if(empAttendance == IS_PART_TIME) {
-        empHrs=4;
-   }
-   else if(empAttendance == IS_FULL_TIME) {
-			empHrs=8;
-   }
-	else
-			empHrs=0;
-
- 
-  int salary=empHrs * EMP_RATE_PER_HR;
-  System.out.println("Salary: "+salary);
+   switch (empAttendance) {
+   case 1:
+           empHrs=4;
+           System.out.println("Employee hours: "+empHrs);
+           salary=(empHrs*EMP_RATE_PER_HR);
+           System.out.println("Salary: "+salary);
+           break;
+   case 2:
+           empHrs=8;
+           System.out.println("Employee hours: "+empHrs);
+           salary=(empHrs*EMP_RATE_PER_HR);
+           System.out.println("Salary: "+salary);
+           break;
+   default:
+           empHrs=0;
+           System.out.println("Employee hours: "+empHrs);
+           salary=(empHrs*EMP_RATE_PER_HR);
+           System.out.println("Salary: "+salary);
+           break;
+    }
   }
 }
