898b2007bb23841fe1d537f2be6596e09c410326
praveen1466 <praveensknl7@gmail.com>
2020-11-27 10:06:04 -0800

    Added: Employee daily wages is updated

--- empWageComputation.java
+++ empWageComputation.java
@@ -4,33 +4,35 @@ public static void main(String[] args) {
 	System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 
    int empHrs;
-   int salary;
    int IS_PART_TIME=1;
    int IS_FULL_TIME=2;
    int EMP_RATE_PER_HR=20;
+   int MAX_HRS_IN_MONTH=10;
    int NUM_WORKING_DAYS=20;
-   int totalSalary=0;
-
-   for(int day=1;day<=NUM_WORKING_DAYS;day++ )
-   {
-    Random rand=new Random();
-    int empAttendance=rand.nextInt(3);
-    switch (empAttendance) {
-    case 1:
-           empHrs=4;
-           salary=(empHrs*EMP_RATE_PER_HR);
-           break;
-    case 2:
-           empHrs=8;
-           salary=(empHrs*EMP_RATE_PER_HR);
-           break;
-    default:
-           empHrs=0;
-           salary=(empHrs*EMP_RATE_PER_HR);
-           break;
-    }
-    totalSalary=(totalSalary+salary);
-   }
-   System.out.println("Total Salary: "+totalSalary); 
+   int totalEmpHrs=0;
+   int totalWorkingDays=0;
+   
+   while( totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_WORKING_DAYS ) {
+         totalWorkingDays++;
+         
+         Random random=new Random();
+         int empAttendance=random.nextInt(3);
+         switch (empAttendance) {
+          case 1:
+                 empHrs=4;
+                 break;
+          case 2:
+                 empHrs=8;
+                 break;
+         default:
+                 empHrs=0;
+                  break;
+        }
+   totalEmpHrs=(totalEmpHrs + empHrs);
+   System.out.println("Total Employee Hours of a Month: "+totalEmpHrs);
 }
+   int totalSalary=(totalEmpHrs*EMP_RATE_PER_HR);
+   System.out.println("Total Salary of a Employee: "+totalSalary); 
+ 
+  }
 }
