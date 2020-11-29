3b8d4b899728808fe4449a066465bc578e0f2136
praveen1466 <praveensknl7@gmail.com>
2020-11-28 22:07:07 -0800

    Refractored: Manage employee for multiple companies

--- empWageComputation.java
+++ empWageComputation.java
@@ -1,68 +1,54 @@
-package employeewage;
+package employeewageusecase;
 
-import java.util.Random;
+class CompanyEmpWage {
 
-public class EmployeeWageMethod {
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
 
-	public static final int IS_PART_TIME = 1;
-	public static final int IS_FULL_TIME = 2;
+	public int totalEmpWage;
 
-	private final String company;
-	private final int empRatePerHour;
-	private final int numOfWorkingDays;
-	private final int maxHoursPerMonth;
-
-	private int totalEmpWage;
-
-	public EmployeeWageMethod(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		super();
+	public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		this.company = company;
-		this.empRatePerHour = empRatePerHour; // 20
-		this.numOfWorkingDays = numOfWorkingDays; // 2
-		this.maxHoursPerMonth = maxHoursPerMonth; // 10
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
 	}
 
-	public void computeEmpWage() {
-		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-		Random random = new Random();
-		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
-			totalWorkingDays++;
-
-			int empCheck = random.nextInt(3);
-			switch (empCheck) {
-			case IS_PART_TIME:
-				empHrs = 4;
-				break;
-
-			case IS_FULL_TIME:
-				empHrs = 8;
-				break;
-			default:
-				empHrs = 0;
-			}
-			totalEmpHrs += empHrs;
-			System.out.println("day: " + totalWorkingDays + "EmpHrs: " + empHrs);
-		}
-		totalEmpWage = totalEmpHrs * empRatePerHour;
-
-
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
 	}
 
 	@Override
 	public String toString() {
 		return "Total Emp Wage for Company: " + company + " is " + totalEmpWage;
 	}
+package employeewage;
 
-	public static void main(String[] args) {
-		EmployeeWageMethod dMart = new EmployeeWageMethod("DMart", 20, 2, 10);
-		EmployeeWageMethod reliance = new EmployeeWageMethod("Reliance", 10, 4, 20);
+class CompanyEmpWage {
 
-		dMart.computeEmpWage();
-		System.out.println(dMart);
+	public final String company;
+	public final int empRatePerHour;
+	public final int numOfWorkingDays;
+	public final int maxHoursPerMonth;
 
-		reliance.computeEmpWage();
-		System.out.println(reliance);
+	public int totalEmpWage;
 
+	public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHour = empRatePerHour;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
+
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Employee Wage for Company: " + company + " is " + totalEmpWage;
 	}
 
 }
